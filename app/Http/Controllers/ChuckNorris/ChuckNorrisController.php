0f44c01de53d059ba9303163654d7efa3fce12c2
Gustavo Bays <dooca@192.168.1.6>
2022-12-04 20:37:54 -0300

    feat: created first route and controllers

---
+++ app/Http/Controllers/ChuckNorris/ChuckNorrisController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\ChuckNorris;
+
+use App\Http\Controllers\BaseController;
+use Illuminate\Http\Request;
+
+class ChuckNorrisController extends BaseController
+{
+}
